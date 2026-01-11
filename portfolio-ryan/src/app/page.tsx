726f02ea6083730e138bd12fd4ccfc95de68c0fa
Ryan Vitor <ryanvitor330@gmail.com>
2026-01-10 21:50:36 -0300

    chore: configuração inicial do projeto com Next.js, Tailwind e bibliotecas

---
+++ portfolio-ryan/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-500">Portfolio Ryan 2.0</h1>
+      <p>Setup concluido com sucesso!</p>
+    </main>
+  );
+}
